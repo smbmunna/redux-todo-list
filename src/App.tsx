c603efe411bcfa84af52bad14145374e59d2d339
Mahmud Munna <smbmunna@gmail.com>
2024-07-09 00:34:51 +0600

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>My todo list using redux</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
